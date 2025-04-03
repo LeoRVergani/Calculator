623219677530c0143f02d68a0cd53a4e12e6828c
leorvergani <vergani@outlook.com.br>
2025-04-03 16:46:23 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Calculator
+{
+    class Program
+    {    
+        static void Main(string[] args)
+        {
+            Console.Clear();
+            Console.WriteLine("Primeiro valor:");
+            float v1 = float.Parse(Console.ReadLine());
+            Console.WriteLine("Segundo valor:");
+            float v2 = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("");
+            Console.WriteLine("Soma: " + (v1 + v2));
+            Console.WriteLine("Subtração: " + (v1 - v2));
+            Console.WriteLine("Multiplicação: " + (v1 * v2));
+            Console.WriteLine("Divisão: " + (v1 / v2));
+        }
+    }
+}
